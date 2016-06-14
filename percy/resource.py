dbe74a1db810bff07758042a159078ddae8642ff
Dmitry Golyshev <d.golyshev@gmail.com>
2016-06-13 20:41:42 -0700

    ResourceLoader build_resources method (#7)
    * ResourceLoader build_resources method
    
    - test data
    - method to build resources
    - tests for it
    
    * Rename src_path to root_dir and make singular

--- percy/resource.py
+++ percy/resource.py
@@ -10,11 +10,8 @@ class Resource(object):
         self.resource_url = resource_url
         if 'sha' in kwargs and 'content' in kwargs or not ('sha' in kwargs or 'content' in kwargs):
             raise ValueError('Exactly one of sha or content is required.')
-        # Encode the content if it exists
-        # TODO Encoding does not work correctly with Python version >= 3.
-        self.content = kwargs['content'].encode('utf-8') if kwargs.get('content') else None
-        self.sha = kwargs.get('sha', utils.sha256hash(self.content))
         self.content = kwargs.get('content')
+        self.sha = kwargs.get('sha', utils.sha256hash(self.content))
         self.is_root = is_root
         self.mimetype = kwargs.get('mimetype')
 
---
+++ percy/resource_loader.py
@@ -0,0 +1,27 @@
+from percy import Resource
+import os
+
+__all__ = ['ResourceLoader']
+
+
+class ResourceLoader(object):
+
+    def __init__(self, root_dir=None, base_url=None):
+        self.root_dir = root_dir
+        self.base_url = base_url
+
+    def build_resources(self):
+        resources = []
+        for root, dirs, files in os.walk(self.root_dir, followlinks=True):
+            for file_name in files:
+                path = os.path.join(root, file_name)
+                with open(path, 'r+') as f:
+                    content = f.read()
+                    path_for_url = path.replace(self.root_dir, '', 1)
+
+                    resource_url = "{0}{1}".format(self.base_url, path_for_url)
+                    resources.append(Resource(resource_url=resource_url, content=content))
+        return resources
+
+    def snapshot_resources(self):
+        pass
