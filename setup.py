857302dd8ab48a91f2989a2e06b724245052a6f4
Tim Haines <tmhaines@gmail.com>
2017-08-22 11:20:12 -0700

    Bump version: 1.0.4 → 1.0.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ test_requirements = [
 
 setup(
     name='percy',
-    version='1.0.4',
+    version='1.0.5',
     description='Python client library for visual regression testing with Percy (https://percy.io).',
     author='Perceptual Inc.',
     author_email='team@percy.io',
